315b5a7472f7f53c1ae6fa5a6852ff0f093ad1b1
Alquser Abdullah <96134552+AlquserAbod@users.noreply.github.com>
2024-06-19 12:43:48 +0300

    ready login page

--- dashboard/src/pages/login/index.tsx
+++ dashboard/src/pages/login/index.tsx
@@ -4,6 +4,15 @@ export const Login = () => {
   return (
     <AuthPage
       type="login"
+      rememberMe={false}
+      registerLink={false}
+      forgotPasswordLink={false}
+      title={(
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center" , gap:"15px"}}>
+          <img src="/NOBG_logo.png" alt="logo" width={"30%"} />
+          <b> Alquser Portfolio Dashboard</b>
+        </div>
+      )}
     />
   );
 };
