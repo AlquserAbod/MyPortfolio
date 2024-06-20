33ebaa3a0e334d58f2646a2c8519e22409f34e4a
Alquser Abdullah <96134552+AlquserAbod@users.noreply.github.com>
2024-06-20 16:29:12 +0300

    test for deploy 2

--- server/server.js
+++ server/server.js
@@ -31,7 +31,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static( path.join( __dirname, 'public' ) ))
 
-
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
 // Routes
