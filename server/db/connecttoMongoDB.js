32938e3b0a5f5b21bd9c140f527e26ec15b4e385
Alquser Abdullah <96134552+AlquserAbod@users.noreply.github.com>
2024-06-20 20:55:24 +0300

    update mongodb uri env variable name

--- server/db/connecttoMongoDB.js
+++ server/db/connecttoMongoDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = connectToMongoDB = async ()=> {
   try {
-    await mongoose.connect(process.env.MONGO_DB_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log("Error connecting to MongoDB",error.message);
