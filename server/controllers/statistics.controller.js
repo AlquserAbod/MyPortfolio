d4dc699e5713e316d7b8a4e164a1514304da3e0d
Alquser Abdullah <96134552+AlquserAbod@users.noreply.github.com>
2024-06-20 16:55:58 +0300

    change file names ( 3 )

--- server/controllers/statistics.controller.js
+++ server/controllers/statistics.controller.js
@@ -1,5 +1,5 @@
 const Category = require('../models/category.model.js');
-const Certificate = require('../models/certification.model.js/index.js');
+const Certificate = require('../models/certification.model.js');
 const Contact = require('../models/contact.model.js');
 const Project = require('../models/project.model.js');
 const Skill = require('../models/skill.model.js');
