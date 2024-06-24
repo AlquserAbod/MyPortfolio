9180d0c7f4d50bf0a54103b197a1cddcff7e0e19
Alquser Abdullah <96134552+AlquserAbod@users.noreply.github.com>
2024-06-24 17:16:19 +0300

    try vercel deployment ( 1 )

--- server/server.js
+++ server/server.js
@@ -67,6 +67,7 @@ app.use('/api/social-links', socialLinksRoutes);
 connectToMongoDB();
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT,() => {
   console.log(`server Running on port ${PORT}`)
 });
