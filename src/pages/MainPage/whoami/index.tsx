3038ef8e06c1b7f86b2844d81ff095f478d4187e
Alquser Abdullah <96134552+AlquserAbod@users.noreply.github.com>
2024-07-16 13:20:28 +0300

    first about me try

--- src/pages/MainPage/whoami/index.tsx
+++ src/pages/MainPage/whoami/index.tsx
@@ -1,13 +1,16 @@
 import { Trans } from 'react-i18next'
 import styles from './whoami.module.scss'
-import nobgLogoUrl from '@/assets/images/logos/noBgLogo.png';
+import hlaflogo from '@/assets/images/logos/hlaflogo.png';
+
 const WhoamiSection = () => {
   return (
     <div className={styles.conatiner}>
       <div className={styles.holder}>
+
         <div className={styles.titleHolder}>
           <div className={styles.title}><Trans i18nKey={'whoami.title'}></Trans></div>
         </div>
+
         <div className={styles.contentHolder}>
           <div className={styles.line1holder}> 
             <Trans i18nKey={"whoami.line1"} components={{span: <span />}}></Trans>
@@ -26,7 +29,7 @@ const WhoamiSection = () => {
         </div>
 
         <div className={styles.logoHolder}>
-          <img src={nobgLogoUrl} className={styles.logo} />
+          <img src={hlaflogo} className={styles.logo} />
         </div>
       </div>
     </div>
